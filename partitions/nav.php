cf0d8c06411b6f33308f4c5faebee1e6c4bb275c
Pedro Kauã de Barros Leite <123245257+PedroLeite321@users.noreply.github.com>
2023-05-12 16:50:26 -0300

    Update nav.php

--- partitions/nav.php
+++ partitions/nav.php
@@ -8,7 +8,7 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a  <a href="/" class="<?= urlIs('/') ? 'bg-gray-300 text-gray-900' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/" class="<?= urlIs('/') ? 'bg-gray-300 text-gray-900' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
               <a href="./about" class="<?= urlIs('/about') ? 'bg-gray-300 text-gray-900' : 'text-gray-300'?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
               <a href="./contact" class="<?= urlIs('/contact') ? 'bg-gray-300 text-gray-900' : 'text-gray-300'?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
             </div>
